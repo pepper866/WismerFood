9101e6b345530f733b3de8a94237b837eb0d5bac
Matthew Parias <maparias@ursinus.edu>
2020-12-02 18:27:04 -0500

    code
    code for apllication

---
+++ Wismer/Wismer/app/src/main/java/com/example/wismer/tresMenu.java
@@ -0,0 +1,14 @@
+package com.example.wismer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tresMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tres_menu);
+    }
+}
